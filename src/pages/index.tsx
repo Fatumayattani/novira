4635a52ef27036c10850b714c2a6bef69135c496
Fatumayattani <fyattani@gmail.com>
2025-08-29 11:22:18 +0300

    fix: correct errors in app.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { StorytellingStudio } from "@/components/storytelling/StorytellingStudio";
+import { Navbar } from "@/components/ui/navbar";
+import { Footer } from "@/components/ui/footer";
+
+const Index = () => {
+  return (
+    <>
+      <Navbar />
+      <StorytellingStudio />
+      <Footer />
+    </>
+  );
+};
+
+export default Index;
